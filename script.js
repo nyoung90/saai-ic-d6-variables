9ef7cecdb47c5b98e0eebce913713316738d4aa7
nyoung90 <musicislovellc@gmail.com>
2024-11-07 03:41:31 +0000

    added JA

---
+++ script.js
@@ -0,0 +1,12 @@
+let favoriteFood = "tacos"
+console.log(favoriteFood)
+
+favoriteFood = "seafood"
+console.log(favoriteFood)
+
+const favoriteHoliday = "Christmas"
+console.log(favoriteHoliday)
+
+console.log("favoriteFood", favoriteFood)
+console.log("favoriteHoliday", favoriteHoliday)
+
